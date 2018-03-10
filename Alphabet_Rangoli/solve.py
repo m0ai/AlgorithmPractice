a82f0e735db024d2c79057dd997a080331cd5659
moai <moai@choeyeongjun-ui-MacBook-Pro-2016.local>
2018-03-10 22:51:54 +0900

    Added Alphabet_Rangoli solve.py with REAME.md

---
+++ Alphabet_Rangoli/solve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+
+import string
+
+def print_rangoli(size):
+    if 0 < size and size > 27:
+        return -1
+    print (size)
+    mid = size - 1
+    table = list()
+
+    for i in range(size-1, 0, -1):
+        row = ['-'] * (size * 2 - 1)
+        for j in range(size-i):
+            row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+        print('-'.join(row))
+
+    for i in range(size):
+        row = ['-'] * (size * 2 - 1)
+        for j in range(size-i):
+            row[mid - j] = row[mid + j] = string.ascii_lowercase[j + i]
+        print('-'.join(row))
+
+if __name__ == '__main__':
+    print_rangoli(30)
