25a21cf1548ced18c24522b40d90e9ee96ea058c
m0ai <frozenmoai@gmail.com>
2018-06-21 15:53:55 +0900

    Remove dummpy file

--- pseudo-isomorphic/solve.py
+++ pseudo-isomorphic/solve.py
@@ -1,60 +1,33 @@
 #!/bin/python
 
+
+# challenges link  - www.hackerrank.com/challenges/pseudo-isomorphic-substrings/problem
+
 from __future__ import print_function
+
 
 import os
 import sys
-
-
-def extractCases(s, r=2):
-    print("Called extractCases Function with s : %s" % s)
-    return
-
-def remove_duplicate_pattern(list_data):
-    print("Called remove_duplicate_pattern Function ")
-    print(list_data)
-
-    minimum = min(list_data)
-    print(minimum)
-    return
-
-# a
-# ab a
-
-import math
 def pseudoIsomorphicSubstrings(s):
     len_each_case = list()
-    cases = set(list())
+    learned_pattern = list()
     for cur_index in range(1, len(s)+1):
-
-        cases = set(list())
         splited_str = s[:cur_index]
-        learned_pattern = list()
+        for read_length in reversed(range(cur_index)):
+            char = splited_str[read_length:cur_index]
+            parsed_pattern = list()
+            pattern_match_dict = {}
+            new_pattern = 0
+            for c in char:
+                if c not in pattern_match_dict:
+                    pattern_match_dict[c] = new_pattern
+                    new_pattern += 1
+                parsed_pattern.append(pattern_match_dict[c])
 
+            if parsed_pattern not in learned_pattern:
+                learned_pattern.append(parsed_pattern)
 
-        str_len = len(splited_str)
-        for read_length in range(1,str_len+1):
-            for index in range(str_len):
-                if index + read_length > str_len:
-                    break
-
-
-                char = splited_str[index:index+read_length]
-                parsed_pattern = list()
-                pattern_match_dict = {}
-                new_pattern = 0
-                for c in char:
-                    if c not in pattern_match_dict:
-                        pattern_match_dict[c] = new_pattern
-                        new_pattern += 1
-                    parsed_pattern.append(pattern_match_dict[c])
-
-                if parsed_pattern not in learned_pattern:
-                    learned_pattern.append(parsed_pattern)
-                    cases.add(char)
-
-        len_each_case.append(len(cases))
-    return len_each_case
+        yield len(learned_pattern)
 
 
 if __name__ == '__main__':
@@ -63,7 +36,6 @@ if __name__ == '__main__':
     #s = raw_input()
 
     s = "bbbbbcabccbabbbaaabcbaacbccaabbaacbccaababacccaabccaccbcaacbbbccccbcaaaaccacaccaabacaaaacabcbabaaabcb"
-
     result = pseudoIsomorphicSubstrings(s)
 
     '''
