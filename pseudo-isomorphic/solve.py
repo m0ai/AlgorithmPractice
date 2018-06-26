2f0053137b2750a747d204fc460690fcccf090d8
m0ai <frozenmoai@gmail.com>
2018-06-27 02:21:36 +0900

    Remove unused variable

--- pseudo-isomorphic/solve.py
+++ pseudo-isomorphic/solve.py
@@ -1,27 +1,35 @@
 #!/bin/python
-
-
-# challenges link  - www.hackerrank.com/challenges/pseudo-isomorphic-substrings/problem
+# -- challenges link --
+# hackerrank.com/challenges/pseudo-isomorphic-substrings/problem
+#
+# - summary
+#
+# you are given a string S, consisting of no more than 105
+# lowercase alphabetical characters. For every prefix of S
+# denoted by S', you are expected to find the size of
+# the largest possible set of strings ,
+# such that all elements of the set are substrings of S'
+# and no two strings inside the set are pseudo-isomorphic to each other.
 
 from __future__ import print_function
 
-
 import os
 import sys
+
 def pseudoIsomorphicSubstrings(s):
-    len_each_case = list()
     learned_pattern = list()
     for cur_index in range(1, len(s)+1):
         splited_str = s[:cur_index]
         for read_length in reversed(range(cur_index)):
             char = splited_str[read_length:cur_index]
+
             parsed_pattern = list()
             pattern_match_dict = {}
-            new_pattern = 0
+            new_char = 0
             for c in char:
                 if c not in pattern_match_dict:
-                    pattern_match_dict[c] = new_pattern
-                    new_pattern += 1
+                    pattern_match_dict[c] = new_char
+                    new_char += 1
                 parsed_pattern.append(pattern_match_dict[c])
 
             if parsed_pattern not in learned_pattern:
@@ -32,12 +40,11 @@ def pseudoIsomorphicSubstrings(s):
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     #s = raw_input()
-
     s = "bbbbbcabccbabbbaaabcbaacbccaabbaacbccaababacccaabccaccbcaacbbbccccbcaaaaccacaccaabacaaaacabcbabaaabcb"
     result = pseudoIsomorphicSubstrings(s)
 
+    # for solving
     '''
     fptr.write('\n'.join(map(str, result)))
     fptr.write('\n')
